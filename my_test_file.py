8f64e17ed45ffea3b4b20037c172e3f6829eada7
Faith Lovell <faithlxvell@gmail.com>
2023-10-21 05:30:17 -0400

    merge conflict example code

--- my_test_file.py
+++ my_test_file.py
@@ -1,2 +1,8 @@
-#Some text in this file to push
-#Fun fact: I draw/ write my smiley faces with pointy noses, like this: :^)
+# Some text in this file to push
+# Fun fact: I draw/ write my smiley faces with pointy noses, like this: :^)
+
+def create_conflict(name: str) -> str:
+    return name + " just created a merge conflict!"
+
+
+print(create_conflict("Faith"))
