4f205ca162daa2aff7a1ae38203861369db156a0
Faith Lovell <faithlxvell@gmail.com>
2023-10-21 04:33:57 -0400

    branching example (+ demonstrating running code)

--- my_test_file.py
+++ my_test_file.py
@@ -1,2 +1,11 @@
-#Some text in this file to push
-#Fun fact: I draw/ write my smiley faces with pointy noses, like this: :^)
+# Some text in this file to push
+# Fun fact: I draw/ write my smiley faces with pointy noses, like this: :^)
+
+# define function above main
+def example_function(branch_name: str) -> str:
+    return "I am in branch: " + branch_name
+
+
+# run code in main
+if __name__ == "__main__":
+    print(example_function("branching-example"))
