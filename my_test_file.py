8db96ead86be04c012f4a64f6797201ceac2dd6c
Faith Lovell <94004626+faithlovell@users.noreply.github.com>
2023-11-01 21:09:32 -0400

    Delete my_test_file.py
    empty repo for Thursday lecture

--- my_test_file.py
+++
@@ -1,8 +0,0 @@
-# Some text in this file to push
-# Fun fact: I draw/ write my smiley faces with pointy noses, like this: :^)
-
-def create_conflict(name: str) -> str:
-    return name + " just created a merge conflict!"
-
-
-print(create_conflict("Faith"))
